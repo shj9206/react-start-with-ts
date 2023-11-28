b4ded66074fd24adf85dad619deeadae1d163f6a
shj9206 <shj9206@gmail.com>
2023-11-28 17:25:14 +0900

    apiService setting

---
+++ src/utils/apiService/sampleService.ts
@@ -0,0 +1,74 @@
+import axios from "axios";
+import APIBuilder from "@/utils/apiService/APIBuilder";
+import AxiosMockAdapter from "axios-mock-adapter";
+import { faker } from "@faker-js/faker";
+import type { SexType } from "@faker-js/faker";
+
+type Response<T> = { data: T };
+type BookListResponse = unknown;
+
+interface User {
+  _id: string;
+  avatar: string;
+  birthday: Date;
+  email: string;
+  firstName: string;
+  lastName: string;
+  sex: SexType;
+  subscriptionTier: string;
+}
+
+function createRandomUser(): User[] {
+  const sex = faker.person.sexType();
+  const firstName = faker.person.firstName(sex);
+  const lastName = faker.person.lastName();
+  const email = "email@sample.com";
+
+  const results = [...Array(23)].map(() => {
+    return {
+      _id: faker.string.uuid(),
+      avatar: faker.image.avatar(),
+      birthday: faker.date.birthdate(),
+      email,
+      firstName,
+      lastName,
+      sex,
+      subscriptionTier: faker.helpers.arrayElement([
+        "free",
+        "basic",
+        "business",
+      ]),
+    };
+  });
+  return results;
+}
+
+const mock = new AxiosMockAdapter(axios);
+
+mock.onGet("/posts").reply(() => {
+  try {
+    const results = createRandomUser();
+    return [200, results];
+  } catch (error) {
+    console.error(error);
+    return [500, { message: "Internal server error" }];
+  }
+});
+mock.onAny().passThrough();
+
+export const testPost = async () => {
+  const api = APIBuilder.get("/posts").withCredentials(true).build();
+  const { data } = await api.call<Response<BookListResponse>>();
+  return data;
+};
+
+export const searchBookList = async (name?: string, size?: number) => {
+  const api = APIBuilder.get(
+    `/api/v1/search/book.json?query=${name}&display=10&start=1`
+  )
+    .withCredentials(true) // 이제 401 에러가 나는 경우, 자동으로 에러를 탐지하는 인터셉터를 사용하게 된다
+    .params({ name, size }) // body가 없는 axios 객체도 빌더 패턴으로 쉽게 만들 수 있다
+    .build();
+  const { data } = await api.call<Response<BookListResponse>>();
+  return data;
+};
--- src/views/apiTest/ApiTest.tsx
+++ src/views/apiTest/ApiTest.tsx
@@ -1,24 +1,22 @@
-import APIBuilder from "@/utils/APIBuilder";
+import { searchBookList, testPost } from "@/utils/apiService/sampleService";
 import { useState, ChangeEvent } from "react";
 export default function ApiTest() {
   // ex
-  type Response<T> = { data: T };
-  type BookListResponse = unknown;
 
   const [inputValue, setInputValue] = useState("");
 
-  const searchBookList = async (name?: string, size?: number) => {
-    const api = APIBuilder.get(
-      `/api/v1/search/book.json?query=${name}&display=10&start=1`
-    )
-      .withCredentials(true) // 이제 401 에러가 나는 경우, 자동으로 에러를 탐지하는 인터셉터를 사용하게 된다
-      .params({ name, size }) // body가 없는 axios 객체도 빌더 패턴으로 쉽게 만들 수 있다
-      .build();
-    const { data } = await api.call<Response<BookListResponse>>();
-    return data;
-  };
   const onChangeFunc = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+  };
+  const apiCallTest = () => {
+    testPost().then((res) => {
+      console.log(res);
+    });
+  };
+  const onClickEvent = (query: string) => {
+    searchBookList(query).then((res) => {
+      console.log(res);
+    });
   };
   return (
     <>
@@ -34,8 +32,13 @@ export default function ApiTest() {
         />
       </p>
       <p>
-        <button type="submit" onClick={() => searchBookList(inputValue)}>
+        <button type="submit" onClick={() => onClickEvent(inputValue)}>
           call
+        </button>
+      </p>
+      <p>
+        <button type="submit" onClick={() => apiCallTest()}>
+          test
         </button>
       </p>
     </>
