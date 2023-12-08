de2effc22e43860110fa6fa6f2184f9e2f50f4b2
shj9206 <shj9206@gmail.com>
2023-12-08 14:59:41 +0900

    layout 적용 테스트

--- src/views/sample/gridSample/Index.tsx
+++ src/views/sample/gridSample/Index.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import CommonGrid, {
-  CommonGridProps,
-} from "@/components/kendo/grid/CommonGrid.tsx";
+import CommonGrid from "@/components/kendo/grid/CommonGrid.tsx";
+import { CommonGridProps } from "@/components/kendo/grid/gridInterfaces.ts";
 import type {
   AccountResult,
   Company,
@@ -11,18 +10,33 @@ import { getCompaniesList } from "@/utils/apiService/accountService.ts";
 
 export default function Index() {
   const column = [
-    {field: "name", title: "name", width: 200, align: "center"},
-    {field: "foundedYear", title: "foundedYear", width: 200, align: "center"},
-    {field: "city", title: "city", width: 200, filterType: "select"},
-    {field: "state", title: "state", width: 200},
-    {field: "country", title: "country", width: 200},
-    {field: "zipCd", title: "zipCd", width: 200, filterType: "multiSelectCheck"},
-    {field: "street", title: "street", width: 200, filterable: false},
-    {field: "adminFirstName", title: "adminFirstName", width: 200, filterable: false},
-    {field: "adminLastName", title: "adminLastName", width: 200, filterable: false},
-    {field: "adminEmail", title: "adminEmail", width: 200, filterable: false},
-    {field: "modDate", title: "modDate", width: 200, filterable: false},
-    {field: "branchCnt", title: "branchCnt", width: 200, filterable: false},
+    { field: "name", title: "name", width: 200, align: "center" },
+    { field: "foundedYear", title: "foundedYear", width: 200, align: "center" },
+    { field: "city", title: "city", width: 200, filterType: "select" },
+    { field: "state", title: "state", width: 200 },
+    { field: "country", title: "country", width: 200 },
+    {
+      field: "zipCd",
+      title: "zipCd",
+      width: 200,
+      filterType: "multiSelectCheck",
+    },
+    { field: "street", title: "street", width: 200, filterable: false },
+    {
+      field: "adminFirstName",
+      title: "adminFirstName",
+      width: 200,
+      filterable: false,
+    },
+    {
+      field: "adminLastName",
+      title: "adminLastName",
+      width: 200,
+      filterable: false,
+    },
+    { field: "adminEmail", title: "adminEmail", width: 200, filterable: false },
+    { field: "modDate", title: "modDate", width: 200, filterable: false },
+    { field: "branchCnt", title: "branchCnt", width: 200, filterable: false },
   ];
   const [commonGridProps, setCommonGridProps] = useState<CommonGridProps>({
     columnHeader: column,
