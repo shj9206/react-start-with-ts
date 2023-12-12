c9d17622bafde846feafe615a794a15900b0a5e9
shj9206 <shj9206@gmail.com>
2023-12-12 17:15:36 +0900

    statusBar 알람팝업 추가, user dropdown리스트 추가

--- src/components/menu/StatusBar.tsx
+++ src/components/menu/StatusBar.tsx
@@ -2,19 +2,23 @@ import {
   AppBar,
   AppBarSection,
   AppBarSpacer,
-  Avatar,
 } from "@progress/kendo-react-layout";
+import { useTranslation } from "react-i18next";
 import {
   DropDownList,
   DropDownListChangeEvent,
 } from "@progress/kendo-react-dropdowns";
 import { Badge, BadgeContainer } from "@progress/kendo-react-indicators";
 import { SvgIcon } from "@progress/kendo-react-common";
-import { bellIcon } from "@progress/kendo-svg-icons";
-import { useState } from "react";
+import { bellIcon, userIcon } from "@progress/kendo-svg-icons";
+import { useRef, useState } from "react";
+import { DropDownButton } from "@progress/kendo-react-buttons";
+import { Popup } from "@progress/kendo-react-popup";
 
 export default function StatusBar() {
-  const categories = [
+  const { i18n } = useTranslation("translation");
+  const anchor = useRef<HTMLButtonElement | null>(null);
+  const regions = [
     "HQ",
     "LG Region Admin",
     "Company Admin",
@@ -22,52 +26,94 @@ export default function StatusBar() {
     "Employee",
     "View Only",
   ];
-  const kendokaAvatar =
-    "https://www.telerik.com/kendo-react-ui-develop/components/images/kendoka-react.png";
+  const locales = ["en", "kr"];
+  const user = ["My Account", "Change Password", "Sigh in History", "Sign Out"];
 
   const [state, setState] = useState({
-    value: categories[0],
+    region: regions[0],
+    locale: locales[0],
+    alamShow: false,
   });
-  const handleChange = (event: DropDownListChangeEvent) => {
+  const handleChangeRegion = (event: DropDownListChangeEvent) => {
     setState((prevState) => ({
       ...prevState,
-      value: event.target.value,
+      regions: event.target.value,
     }));
   };
-  return (
-    <AppBar>
-      <AppBarSection>
-        <h1 className="title">Enblock Manager</h1>
-      </AppBarSection>
-      <AppBarSpacer style={{ width: 4 }} />
-      <DropDownList
-        style={{ width: "150px" }}
-        data={categories}
-        value={state.value}
-        onChange={(e) => handleChange(e)}
-      />
-      <AppBarSpacer />
-      <AppBarSection className="actions">
-        <button
-          className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base"
-          aria-label="1"
-          type="button"
-        >
-          <BadgeContainer>
-            <SvgIcon icon={bellIcon} />
-            <Badge themeColor="info" size="small" position="inside" />
-          </BadgeContainer>
-        </button>
-      </AppBarSection>
-      <AppBarSection>
-        <span className="k-appbar-separator" />
-      </AppBarSection>
+  const handleChangeLocale = (e: DropDownListChangeEvent) => {
+    const lng = e.target.value;
+    setState((prevState) => ({
+      ...prevState,
+      locale: e.target.value,
+    }));
+    i18n.changeLanguage(lng);
+  };
+  const onClickAlamPopup = () => {
+    setState((prevState) => ({
+      ...prevState,
+      alamShow: !state.alamShow,
+    }));
+  };
 
-      <AppBarSection>
-        <Avatar type="image">
-          <img src={kendokaAvatar} alt="KendoReact Layout Kendoka Avatar" />
-        </Avatar>
-      </AppBarSection>
-    </AppBar>
+  return (
+    <>
+      <AppBar>
+        <AppBarSection>
+          <h1 className="title">Enblock Manager</h1>
+        </AppBarSection>
+        <AppBarSpacer style={{ width: 4 }} />
+        <DropDownList
+          style={{ width: "150px" }}
+          data={regions}
+          value={state.region}
+          onChange={(e) => handleChangeRegion(e)}
+        />
+        <AppBarSpacer />
+        <AppBarSection className="actions">
+          <button
+            className="k-button k-button-md k-rounded-md k-button-flat k-button-flat-base"
+            aria-label="1"
+            type="button"
+            ref={anchor}
+            onClick={() => onClickAlamPopup()}
+          >
+            <BadgeContainer>
+              <SvgIcon icon={bellIcon} />
+              <Badge themeColor="info" size="small" position="inside" />
+            </BadgeContainer>
+            <Popup
+              anchor={anchor.current}
+              show={state.alamShow}
+              popupClass="popup-content"
+            >
+              Popup content.
+            </Popup>
+          </button>
+        </AppBarSection>
+        <AppBarSection>
+          <span className="k-appbar-separator" />
+        </AppBarSection>
+
+        <AppBarSection>
+          <DropDownButton items={user} svgIcon={userIcon} />
+        </AppBarSection>
+
+        <AppBarSpacer style={{ width: 4 }} />
+        <DropDownList
+          style={{ width: "70px" }}
+          data={locales}
+          value={state.locale}
+          onChange={(e) => handleChangeLocale(e)}
+        />
+      </AppBar>
+      <style>
+        {` .popup-content {
+            padding: 250px;
+            color: #787878;
+            background-color: #fcf7f8;
+            border: 1px solid rgba(0,0,0,.05);
+        }`}
+      </style>
+    </>
   );
 }
