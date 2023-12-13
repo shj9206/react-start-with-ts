62310a76bb38caa531d94b707b4a9dcc4f7ea411
shj9206 <shj9206@gmail.com>
2023-12-13 17:10:32 +0900

    dashboard card 개발중

--- src/views/dashboard/tiles/GlobalUserNumber.tsx
+++ src/views/dashboard/tiles/GlobalUserNumber.tsx
@@ -1,3 +1,10 @@
 export default function GlobalUserNumber() {
-  return <div>GlobalUserNumber</div>;
+  return (
+    <div>
+      <h3>Total user 1809</h3>
+      <h5>Company admin 300</h5>
+      <h5>Branch admin 150</h5>
+      <h5>Employee 1500</h5>
+    </div>
+  );
 }
